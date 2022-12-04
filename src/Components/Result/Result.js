73e9bc457aa4b3b9924642fc0ed5af3b4b1ae616
Scott Gaines <scottdgaines@gmail.com>
2022-12-04 15:54:21 -0700

    Add component structure for AddBook children

---
+++ src/Components/Result/Result.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+      <p>I'm a search result</p>
+    </div>
+  )
+}
+
+export default Result
--- src/Pages/AddBook/AddBook.js
+++ src/Pages/AddBook/AddBook.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Result from '../../Components/Result/Result'
 
 const AddBook = () => {
+  // const result = array.map(result => {
+  //   return (
+  //     <Result />
+  //   )
+  // })
+
   return (
     <div>
       <p>Add a book</p>
+      {/* {result} */}
     </div>
   )
 }
