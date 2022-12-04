101d9a8ed4ee7142a7ce693e985fef4119b70da5
Scott Gaines <scottdgaines@gmail.com>
2022-12-04 15:43:57 -0700

    Build out Component structure for Dashboard children

---
+++ src/Components/Shelf/Shelf.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cover from '../Cover/Cover'
+
+const Shelf = () => {
+    //map over array of books to produce a Cover component of each one
+    
+  return (
+    <div>
+      <p>here's a shelf</p>
+      <Cover />
+    </div>
+  )
+}
+
+export default Shelf
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Shelf from '../../Components/Shelf/Shelf'
 
 const Dashboard = () => {
   return (
     <div>
-      <p>Dashboard</p>
+      <Shelf />
+      <Shelf />
     </div>
   )
 }
