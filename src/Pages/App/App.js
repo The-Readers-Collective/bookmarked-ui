2bc0f6be48638b1e2f06d2411ec8e9ca04a4379a
Scott Gaines <scottdgaines@gmail.com>
2022-12-05 16:58:45 -0700

    Added conditional rendering of page names and home links

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useQuery, gql } from '@apollo/client'
-import { Route, Switch } from 'react-router-dom'
+import { Link, Route, Switch, useLocation } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard'
 import BrowseAllBooks from '../BrowseAllBooks/BrowseAllBooks'
 import SingleBookView from '../SingleBookView/SingleBookView'
@@ -8,7 +8,22 @@ import AddBook from '../AddBook/AddBook'
 import PageNotFound from '../PageNotFound/PageNotFound'
 
 const App = () => {
-  const pageName = "name"
+  const location = useLocation()
+  const homeLink = location.pathname != "/" && <Link to="/">Return Home</Link>
+  let pageName;
+
+  if (location.pathname === "/") {
+    pageName = "My Bookshelf"
+  } else if (location.pathname === "/browse") {
+    pageName = "Browse All Books"
+  } else if (location.pathname === "/add") {
+    pageName = "Add a Book"
+  } else if (location.pathname === "/:id") {
+    pageName = "Book View"
+  } else {
+    pageName = "oops"
+  }
+  //Once we have single book view working, test that lines 21 and 22 are functional
 
   return (
     <div>
@@ -16,6 +31,7 @@ const App = () => {
         <h1>Bookmarked</h1>
         <h2>tagline here</h2>
         <h3>{pageName}</h3>
+        <h3>{homeLink}</h3>
       </nav>
       <main>
         <Switch>
@@ -28,16 +44,17 @@ const App = () => {
           <Route path="/add" render={() =>
             <AddBook />
           }/>
-          <Route path="/:ISBN" render={() => 
-            <SingleBookView />
-          }/>
           <Route path="*" render={() => 
             <PageNotFound />
           }/>
+          <Route path="/:id" render={() => 
+            <SingleBookView />
+          }/>
+     
         </Switch>
       </main>
       <footer>
-        <p>link to Reader's Collective</p>
+        <p>Visit us at <a href="https://github.com/The-Readers-Collective">The Reader's Collective</a></p>
       </footer>
     </div>
 
