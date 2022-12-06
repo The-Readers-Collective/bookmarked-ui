1893b57ba7befb9ee01aa7d51917be39f2458a9c
Scott Gaines <scottdgaines@gmail.com>
2022-12-05 17:00:10 -0700

    Add cy and styling attributes, add buttons

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Shelf from '../../Components/Shelf/Shelf'
 
 const Dashboard = () => {
 
   return (
-    <div>
-      <Shelf />
+    <div data-cy="bookshelves-container" className="bookshelves-container">
+      <Link to="/browse">
+        <button data-cy="browse-button" className="button">Browse</button>
+      </Link>
+      <Link to="/add">
+        <button data-cy="add-button" className="button">Add a Book</button>
+      </Link>
+      <Shelf myShelfBooks={true} />
       <Shelf />
     </div>
   )
