a3a763312ee351ac2d3c882ffd97e70280ade1c2
Scott Gaines <scottdgaines@gmail.com>
2022-12-06 14:40:49 -0700

    Update app.test.js line 11 to Bookmarked

--- src/Pages/App/App.test.js
+++ src/Pages/App/App.test.js
@@ -8,6 +8,6 @@ test('renders learn react link', () => {
       <App />
     </BrowserRouter>
   );
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Bookmarked");
   expect(linkElement).toBeInTheDocument();
 });
