13088b62ec7ab3aff685063e69cf195e13692a2c
Scott Gaines <scottdgaines@gmail.com>
2022-12-06 14:47:31 -0700

    Add apollo import

--- src/Pages/App/App.test.js
+++ src/Pages/App/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client'
 import App from './App';
 
 test('renders learn react link', () => {
