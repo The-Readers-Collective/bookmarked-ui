dd47d19489322b9cf5bf487800e50d1adfc5d83a
Scott Gaines <scottdgaines@gmail.com>
2022-12-06 20:35:20 -0700

    Build out basic structure for displaying data

--- src/Pages/SingleBookView/SingleBookView.js
+++ src/Pages/SingleBookView/SingleBookView.js
@@ -11,16 +11,16 @@ const SingleBookView = ({fromShelf}) => {
 
     return (
         <div>
-        <p>Single book view</p>
-        <img src={} alt="Book Cover" className="book-cover" />
-        <article>
-            <p>Title:{title}</p>
-            <p>Author:{author}</p>
-            <p>Genre:{genre}</p> 
-            <p>Page Count:{pageCount}</p>
-            <p>Synopsis:{synopsis}</p>
-            <p>Condition:{condition}</p>
-        </article>
+            <p>Single book view</p>
+            {/* <img src={source} alt="Book Cover" className="book-cover" />
+            <article className="book-details-container">
+                <p>Title:{title}</p>
+                <p>Author:{author}</p>
+                <p>Genre:{genre}</p> 
+                <p>Page Count:{pageCount}</p>
+                <p>Synopsis:{synopsis}</p>
+                <p>Condition:{condition}</p>
+            </article> */}
         </div>
     )
 }
