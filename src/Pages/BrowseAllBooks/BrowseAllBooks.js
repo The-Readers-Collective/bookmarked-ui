f39dbcf4b9c1e5e424f3c55e30c2292e9ef4bee5
Scott Gaines <scottdgaines@gmail.com>
2022-12-07 10:09:54 -0700

    Relocate and refactor query-- data coming through)

--- src/Pages/BrowseAllBooks/BrowseAllBooks.js
+++ src/Pages/BrowseAllBooks/BrowseAllBooks.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import SearchAllBooks from '../../Components/SearchAllBooks/SearchAllBooks'
+import { gql, useQuery } from '@apollo/client'
 
 const BrowseAllBooks = () => {
+  const GET_BROWSE_ALL_BOOKS = gql`
+    query GetBrowseAllBooks {
+      users {
+          userBooks {
+            status
+              book {
+                  bookTitle
+                  bookCover
+                  author
+                  available
+              }
+          }
+      }
+  }
+  `;
+
+  const { loading, error, data } = useQuery(GET_BROWSE_ALL_BOOKS)
+  console.log('browse data',  data)
+  console.log('browse error', error)
+
     //const allBrowsedCover = array.map(book => {
     //    return (
     //    <Cover />
     //    )
     // })
+    
   return (
     <div>
       <p>Browse</p>
