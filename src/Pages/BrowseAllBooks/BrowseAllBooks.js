122a60f0aa71d1c87fefbdedb9d9d7b7a65b630f
Scott Gaines <scottdgaines@gmail.com>
2022-12-07 12:05:33 -0700

    Dig into data, commented out iterators to avoid error

--- src/Pages/BrowseAllBooks/BrowseAllBooks.js
+++ src/Pages/BrowseAllBooks/BrowseAllBooks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchAllBooks from '../../Components/SearchAllBooks/SearchAllBooks'
 import { gql, useQuery } from '@apollo/client'
+import Cover from '../../Components/Cover/Cover.js'
 
 const BrowseAllBooks = () => {
   const GET_BROWSE_ALL_BOOKS = gql`
@@ -19,19 +20,24 @@ const BrowseAllBooks = () => {
   }
   `;
 
-  const { loading, error, data } = useQuery(GET_BROWSE_ALL_BOOKS)
-  console.log('browse data',  data)
-  console.log('browse error', error)
+    const { loading, error, data } = useQuery(GET_BROWSE_ALL_BOOKS)
+    console.log('browse data',  data)
+    console.log('browse error', error)
 
-    //const allBrowsedCover = array.map(book => {
-    //    return (
-    //    <Cover />
-    //    )
+    // let allBrowsedCovers;
+    // data.users.forEach(user => {
+    //   allBrowsedCovers = user.userBooks.map(book => {
+    //     return (
+    //       <Cover />
+    //     )
+    //     console.log(32, allBrowsedCovers)
+    //     console.log(user)
+    //   })
     // })
-    
+
   return (
     <div>
-      <p>Browse</p>
+      <p>Browse all books</p>
       <SearchAllBooks />
       {/* {allBrowsedCovers} */}
     </div>
