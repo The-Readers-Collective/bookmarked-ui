2332b7657f67bf4c181373326d0d923fa71f5253
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-08 19:23:26 -0700

    Add syntax to testing

--- cypress/e2e/browsedCollection.cy.js
+++ cypress/e2e/browsedCollection.cy.js
@@ -4,22 +4,35 @@ describe('Browsed Collection', () => {
   })
 
     it('should should see a collection of all the users books', () => {
-      cy.get('h1').should('be.visible')
+      cy.get('[data-cy="browse-books-container"]')
+        .get('[href="/1"] > div > img').should('be.visible')
+        .get('[href="/2"] > div > img').should('be.visible')
     })
 
     it('should be able to hover over a book cover, and click it to view its individual details', () => {
-      cy.get('h1').should('be.visible')
+      cy.get('[data-cy="browse-books-container"]')
+        .get('[href="/1"] > div > img').should('be.visible')
+        .click()
+      
     })
 
     it('should be able to search through the collection by title, author, or genre and reset the search parameters', () => {
-      cy.get('h1').should('be.visible')
+      cy.get('[data-cy="browse-books-container"]')
+        .get('[placeholder="Title"]').type('Calib')
+        .get('[placeholder="Title"]').type('James')
+        .get('select')
+        .get('label')
+        .click()
+        // .click('Sci-Fi')
+        
     })
 
     it('should be able to return back to the main dashboard', () => {
-      cy.get('h1').should('be.visible')
+      cy.get('[data-cy="return-home-text"]')
+      .click()
     })
 
-    it('should be able to see the difference between an available book and an unavailable book', () => {
+    it.skip('should be able to see the difference between an available book and an unavailable book', () => {
       cy.get('h1').should('be.visible')
     })
 
