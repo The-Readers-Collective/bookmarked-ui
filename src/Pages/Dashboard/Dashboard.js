7d6e4f9537ff324e8981e2f5d417b9185762e4e9
Stephanie Guzman <sguzman247@gmail.com>
2022-12-08 21:56:10 -0600

    Update query fields, iterate and return user books by status

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Shelf from '../../Components/Shelf/Shelf'
-// import GET_DASHBOARD from '../../queries.graphql'
 import { useQuery, gql } from '@apollo/client'
 
 
@@ -9,32 +8,37 @@ const Dashboard = () => {
   const GET_DASHBOARD = gql`
     query getDashboardBooks {
       user(id: "1") {
-          id
-          name
-          userBooks {
-              bookId
-              status
-              book {
-                  id
-                  bookTitle
-                  bookCover
-                  author
-                  isbn13
-              }
+        id
+        name
+        userBooks {
+          bookId
+          status
+          book {
+            id
+            bookTitle
+            bookCover
+            author
+            isbn13
+            available
           }
+        }
       }
-  }
-`;
+    }
+  `;
   
   const { loading, error, data } = useQuery(GET_DASHBOARD)
   if (error) return <p>Error : {error.message}</p>
   if (loading) return <p>Loading...</p>
   
+  let owned = []
+  let bookmarked = []
   
   if (data) {
-    console.log(data)
+    const books = data.user.userBooks
+    books.forEach(book => {
+      book.status === 0 ? owned.push(book) : bookmarked.push(book)
+    })
   }
-
 
   return (
     <div data-cy="bookshelves-container" className="bookshelves-container">
@@ -44,8 +48,13 @@ const Dashboard = () => {
       <Link to="/add">
         <button data-cy="add-button" className="button">Add a Book</button>
       </Link>
-      <Shelf myShelfBooks={true} />
-      <Shelf />
+      <Shelf 
+        ownedBooks={owned}
+        myShelfBooks={true} 
+      />
+      <Shelf 
+        bookmarkedBooks={bookmarked}
+      />
     </div>
   )
 }
