2ea46cc02101f9aae1b6c924b4827186444b0b4d
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-09 13:35:24 -0700

    Add search title functionality

--- src/Components/SearchAllBooks/SearchAllBooks.js
+++ src/Components/SearchAllBooks/SearchAllBooks.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import { useState } from 'react';
 import browsedBooks from '../../Pages/BrowseAllBooks/BrowseAllBooks'
 
 const SearchAllBooks = ({ searchTitle, setSearchTitle, filteredSearch }) => {
-  const [results, setResults] = useState([])
-  const [message, setMessage] = useState('Search for a book')
-  const [searchAuthor, setSearchAuthor] = useState('')
-  // const [filterCategory, setFilterCategory] = useState(["All"])
-
   return (
     <form>
       <input 
@@ -19,33 +13,6 @@ const SearchAllBooks = ({ searchTitle, setSearchTitle, filteredSearch }) => {
         onChange={event => setSearchTitle(event.target.value)}    
         >
       </input>
-      {/* <input 
-        type="text" 
-        placeholder='Author'
-        name='author'
-        value={searchAuthor}
-        data-cy='input'
-        onChange={setSearchAuthor(event.target.value)}
-        >
-      </input> */}
-      {/* <label>Genre</label> */}
-      {/* <select
-      onChange={(e) => {
-      // setFilterParam(e.target.value);
-       }}
-       className="custom-select-category"
-       aria-label="Filter books by category">
-        <option value="All">Filter By category</option>
-        <option value="Sci-fi">Sci-fi</option>
-        <option value="Adventure">Adventure</option>
-        <option value="Childrens Book">Childrens Book</option>
-        <option value="Juvenile Fiction">Juvenile Fiction</option>
-        <option value="Fiction">Fiction</option>
-        <option value="Young Adult Fiction">Young Adult Fiction</option>
-        <option value="Magic">Magic</option>
-        <option value="Biography & Autobiography">Biography & Autobiography</option>
-        <option value="Brothers and sisters">Brothers and sisters</option>
-      </select> */}
       <button onClick={event => filteredSearch(event)}>Submit</button>
     </form>
     
--- src/Pages/BrowseAllBooks/BrowseAllBooks.js
+++ src/Pages/BrowseAllBooks/BrowseAllBooks.js
@@ -1,4 +1,3 @@
-// import React from 'react'
 import SearchAllBooks from '../../Components/SearchAllBooks/SearchAllBooks'
 import { gql, useQuery } from '@apollo/client'
 import Cover from '../../Components/Cover/Cover.js'
@@ -6,6 +5,7 @@ import React, { useState } from "react";
 
 const BrowseAllBooks = () => {
   const [searchTitle, setSearchTitle] = useState('')
+  const [results, setResults] = useState([])
   // const [searchAuthor, setSearchAuthor] = useState('')
   const GET_BROWSE_ALL_BOOKS = gql`
     query GetBrowseAllBooks {
@@ -27,7 +27,6 @@ const BrowseAllBooks = () => {
   if (error) return <p>Error : {error.message}</p>
   if (loading) return <p>Loading...</p>
   
-  
   if (data) {
     allBrowsedCovers = data.books.map(userBook => {
       return (
@@ -47,23 +46,35 @@ const BrowseAllBooks = () => {
     const filteredSearch = (event) => {
       event.preventDefault()
       const filterBooks = bookData.filter((book) => {
-        console.log(book)
          return book.bookTitle.toUpperCase().includes(searchTitle.toUpperCase())
       })
-      console.log(filterBooks)
+      setResults(filterBooks)
+      setSearchTitle('')
     }
+
+   const searchResults = results.map((userBook) => {
+    return (
+      <Cover 
+      id={userBook.id}
+      key={userBook.id} 
+      author={userBook.author} 
+      title={userBook.bookTitle} 
+      cover={userBook.bookCover}
+      />
+      )
+   })
    
       return (
         <div data-cy="browse-books-container" className="browse-books-container">
           <p data-cy="browse-header" className="browse-header">Browse all books</p>
+          {console.log(results)}
           <SearchAllBooks 
             searchTitle={searchTitle}
             setSearchTitle={setSearchTitle}
             filteredSearch={filteredSearch}
-            // searchAuthor={searchAuthor}
-            // setSearchAuthor={setSearchAuthor}
           />
-          {allBrowsedCovers}
+          {searchTitle ? searchResults : allBrowsedCovers} 
+
         </div>
   )
 } 
