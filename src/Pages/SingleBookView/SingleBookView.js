08dcf31d5baec24b97d48aed791e8ab36150d00c
Marianne Barton <mhood24@gmail.com>
2022-12-09 22:12:33 -0700

    Add classnames and data-cy to items, add in divs for styling purposes

--- src/Pages/SingleBookView/SingleBookView.js
+++ src/Pages/SingleBookView/SingleBookView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { gql, useQuery } from '@apollo/client'
+import './SingleBookView.css'
 
 
 const SingleBookView = ({fromShelf, id }) => {
@@ -35,17 +36,26 @@ const SingleBookView = ({fromShelf, id }) => {
     }
 
     return (
-        <div>
+        <div data-cy='single-book-container' className='single-book-container'>
             <p>Single book view</p>
-            <button>bookmark!<button>
-            <img  data-cy="book-cover"  src={data.book.bookCover} alt="Book Cover" className="book-cover" />
-            <article className="book-details-container">
-                <p data-cy="book-title" className="book-title">Title: {data.book.bookTitle}</p>
-                <p data-cy="book-author" className="book-author">Author: {data.book.author}</p>
-                <p>Synopsis: {data.book.description}</p> 
+            <div data-cy='top-single-book-container' className='top-single-book-container'>
+                <div data-cy='cover-image-container' className='cover-image-container'>
+                    <img  data-cy="book-cover"  src={data.book.bookCover} alt="Book Cover" className="book-cover" />
+                    <button>bookmark!</button>
+                </div>
+                <article className="book-major-details-container">
+                    <p data-cy="book-title" className="book-title">{data.book.bookTitle}</p>
+                    <p data-cy="book-author" className="book-author">By: {data.book.author}</p>
+                    <p data-cy="book-synopsis" className="book-synopsis">{data.book.description}</p> 
+                </article>
+            </div>
+            <article data-cy='book-minor-details-container' className="book-minor-details-container">
                 <p data-cy="book-details" className="book-details">Genre: {data.book.category}</p> 
+                <p className='divider hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
                 <p data-cy="book-details" className="book-details">Page Count: {data.book.pgCount}</p> 
+                <p className='divider hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
                 <p data-cy="book-details" className="book-details">ISBN: {data.book.isbn13}</p> 
+                <p className='divider hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
                 <p data-cy="book-details" className="book-details">Condition: {data.book.condition}</p>
             </article>
         </div>
