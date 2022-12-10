f363923bb7aaa73bc96e48b7b11c633c4e21fc4b
Stephanie Guzman <sguzman247@gmail.com>
2022-12-10 10:45:12 -0600

    Add intercept, add header, footer, and button routing testing

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -2,32 +2,32 @@ import User from '../fixtures/dashboard.json'
 
   describe('Dashboard', () => {
     beforeEach(() => {
-      cy.visit('localhost:3000')
+      cy.intercept('POST', 'https://bookmarked-api.herokuapp.com/graphql', { fixture: 'dashboard.json' }).as('userBooks')
+    cy.visit('/')
+    cy.wait('@userBooks')
+      cy.visit('/')
+
     })
 
-    it('should have a title', () => {
-      cy.get('h1').should('be.visible')
+    it.skip('should have a title and tagline', () => {
+      cy.get('[data-cy="app-title"]').contains('Bookmarked')
+      cy.get('[data-cy="app-tagline"]').contains('Where Book Lovers Gather')
+
     })
 
-    it('should have a tagline', () => {
-      cy.get('h2').should('be.visible')
-    })
-
-    it('should have a button that takes a user to the browsing collection view', () => {
+    it.skip('should have a button that takes a user to the browsing collection view when clicked', () => {
       cy.get('[data-cy="browse-button"]')
         .click()
-        .get(':nth-child(4) > a')
-        .click()
+        .get('[data-cy="browse-header"]').contains('Browse all books')
     })
 
-    it('should have a button that takes a user to add a book view', () => {
+    it.skip('should have a button that takes a user to add a book view when clicked', () => {
       cy.get('[data-cy="add-button"]')
         .click()
-        .get(':nth-child(4) > a')
-        .click()
+      cy.get('[data-cy="page-name"]').contains('Add a Book')
     })
 
-    it.skip('should display a users shelf', () => {
+    it('should display a users shelf', () => {
       cy.get('card')
        
     })
@@ -47,19 +47,19 @@ import User from '../fixtures/dashboard.json'
         
     })
 
-    it('should be able to use the browser arrow buttons to go between pages', () => {
-      cy.visit("http://localhost:3000/")
+    it.skip('should be able to use the browser arrow buttons to go between pages', () => {
+      cy.visit('/')
       cy.get('[data-cy="browse-button"]')
         .click()
-        .url().should('eq', 'http://localhost:3000/browse')
+        .url().should('eq', '/browse')
         .go('back')
-        .url().should('eq', 'http://localhost:3000/')
+        .url().should('eq', '/')
         .go('forward')
-        .url().should('eq', 'http://localhost:3000/browse')
+        .url().should('eq', '/browse')
     }) 
 
-    it('should display a footer with a link to github', () => {
-      cy.get('[data-cy="footer-text"]').should('be.visible')
+    it.skip('should display a footer with a link to github', () => {
+      cy.get('[data-cy="footer"]').contains(`Visit us at The Reader's Collective`)
 
         
     })
