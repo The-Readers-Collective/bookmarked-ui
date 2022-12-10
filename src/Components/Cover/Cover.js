021a8039d263087a0059367c1773d8e584b8f705
Stephanie Guzman <sguzman247@gmail.com>
2022-12-09 18:52:56 -0600

    Add props to components to render

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import './Cover.css'
 
-const Cover = ({ cover, id, available }) => {
+const Cover = ({ cover, id, available, deleteBook }) => {
   return (
     <Link to={`/${id}`}> 
-      <div data-cy='cover' className='cover'>
+      <div data-cy='cover' className='cover' id={id}>
         <img data-cy='cover-image' className='cover-image' src={cover} alt='book name'/>
+        <button onClick={() => deleteBook(id)}>Delete</button>
       </div>
     </Link>
   )
