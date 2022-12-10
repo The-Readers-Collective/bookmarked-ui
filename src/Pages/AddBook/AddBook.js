63fa401f5c50bbea0bc05a28e7bed22af3f06f65
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-10 10:20:55 -0700

    Add add-book query to add book file

--- src/Pages/AddBook/AddBook.js
+++ src/Pages/AddBook/AddBook.js
@@ -3,12 +3,13 @@ import { gql, useQuery } from '@apollo/client'
 import { useMutation } from '@apollo/client';
 // import Result from '../../Components/Result/Result'
 
-const AddBookForm = ({ refetch }) => {
+const AddBookForm = () => {
   const [bookSearchTerm, setBookSearchTerm] = useState('')
+  const [ searchResults, setSearchResults ] = ([])
 
   const SEARCH_BOOK = gql `
-    query SEARCH_BOOK {
-      googleBooks(bookTitle: String) {
+    query SEARCH_BOOK($title: String!) {
+      googleBooks(title: $title)   {
         bookTitle
         bookCover
         author
@@ -16,21 +17,37 @@ const AddBookForm = ({ refetch }) => {
         description
         googleBookId
         isbn13
-        pageCount
+        pgCount
       }
   }
   `
   
   // const [addBook, { loading, error}] = useMutation(ADD_BOOK)
 
-  const { loading, error, data } = useQuery(SEARCH_BOOK)
-
-  if (error) return <p>Error : {error.message}</p>
-  if (loading) return <p>Loading...</p>
-
-  const clearInputs = () => {
-    setBookSearchTerm('')
-  }
+  
+  const { loading, error, data } = useQuery(SEARCH_BOOK, 
+    {variables: { title: bookSearchTerm }}
+    )
+    
+    if (error) return <p>Error : {error.message}</p>
+    if (loading) return <p>Loading...</p>
+    
+    let singleBookData = data.googleBooks
+      
+    const filteredSearch = (event) => {
+      event.preventDefault()
+      const filterBooks = singleBookData.filter((book) => {
+         return book.bookTitle.toUpperCase().includes(bookSearchTerm.toUpperCase())
+      })
+      setSearchResults(filterBooks)
+    }
+  
+    const clearInputs = () => {
+      setSearchResults([])
+    }
+    // const clearInputs = () => {
+  //   setBookSearchTerm('')
+  // }
 
   // const handleClick = (event) => {
   //   event.preventDefault()
@@ -54,24 +71,26 @@ const AddBookForm = ({ refetch }) => {
   //   clearSearchInput()
   // }
 
-  const clearSearchInput = () => {
-    setBookSearchTerm('')
-  }
+
   
   return (
     <div className='add-book-container' data-cy='add-book-container'>
       <h3 className='title' data-cy='add-book-title'>Search books to add to your shelf!</h3>
+      <img  data-cy="book-cover"  src={data.googleBooks.bookTitle} alt="Book Cover" className="book-cover" />
       <input 
         type="text" 
         placeholder='Book Name'
         value={bookSearchTerm}
         onChange={(event) => setBookSearchTerm(event.target.value)}
       />
+      <button onClick={event => filteredSearch(event)}>Search for results</button>
+      
+      {/* This second button will appear on each individual search result: not on screen until user hits filter button */}
       <button 
         data-cy='search-add-book-btn'> 
+        Add this book to my shelf!
         {/* // onClick={(event)=> handleClick(event)}>Search */}
         </button>
-        <button>Add this book to my shelf!</button>
     </div>
   )
 }
