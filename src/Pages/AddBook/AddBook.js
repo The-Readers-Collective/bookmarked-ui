d4efa93c9e03b0421e50edd9b230a26d275adab0
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-09 19:51:06 -0700

    Add intial code for add book

--- src/Pages/AddBook/AddBook.js
+++ src/Pages/AddBook/AddBook.js
@@ -1,21 +1,58 @@
 import React, { useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import { useMutation } from '@apollo/client';
 // import Result from '../../Components/Result/Result'
 
-const AddBook = () => {
+const AddBookForm = ({ refetch }) => {
   const [bookSearchTerm, setBookSearchTerm] = useState('')
 
-  // const result = array.map(result => {
-  //   return (
-  //     <Result />
-  //   )
-  // })
-
-  const handleClick = (event) => {
-    event.preventDefault()
-    //query Google API 
-    //return Results component with search results
-    clearSearchInput()
+  const SEARCH_BOOK = gql `
+    query SEARCH_BOOK {
+      googleBooks(bookTitle: String) {
+        bookTitle
+        bookCover
+        author
+        category
+        description
+        googleBookId
+        isbn13
+        pageCount
+      }
   }
+  `
+  
+  // const [addBook, { loading, error}] = useMutation(ADD_BOOK)
+
+  const { loading, error, data } = useQuery(SEARCH_BOOK)
+
+  if (error) return <p>Error : {error.message}</p>
+  if (loading) return <p>Loading...</p>
+
+  const clearInputs = () => {
+    setBookSearchTerm('')
+  }
+
+  // const handleClick = (event) => {
+  //   event.preventDefault()
+  //     if (bookSearchTerm) {
+  //       addBook({
+  //         variables: {
+  //           input: {
+  //             bookTitle: `${bookTitle}`,
+  //             bookCover: `${bookCover}`,
+  //             author: `${author}`,
+  //             category: `${category}`,
+  //             description: `${description}`,
+  //             googleBookId: `${googleBookId}`,
+  //             isbn13: `${isbn}`,
+  //             pageCount: `${pageCount}`
+  //           }
+  //         }
+  //       })
+  //     }
+  //   refetch()
+  //   clearSearchInput()
+  // }
 
   const clearSearchInput = () => {
     setBookSearchTerm('')
@@ -23,7 +60,7 @@ const AddBook = () => {
   
   return (
     <div className='add-book-container' data-cy='add-book-container'>
-      <h3 className='title' data-cy='add-book-title'>Add a new book to your shelf</h3>
+      <h3 className='title' data-cy='add-book-title'>Search books to add to your shelf!</h3>
       <input 
         type="text" 
         placeholder='Book Name'
@@ -31,10 +68,12 @@ const AddBook = () => {
         onChange={(event) => setBookSearchTerm(event.target.value)}
       />
       <button 
-        data-cy='search-add-book-btn' 
-        onClick={(event)=> handleClick(event)}>Search</button>
+        data-cy='search-add-book-btn'> 
+        {/* // onClick={(event)=> handleClick(event)}>Search */}
+        </button>
+        <button>Add this book to my shelf!</button>
     </div>
   )
 }
 
-export default AddBook
+export default AddBookForm
