764016e6ec38b6000f2fa4223abc72555ea79e67
Stephanie Guzman <sguzman247@gmail.com>
2022-12-11 11:57:49 -0600

    Update prop naming

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
-
-const Result = ({id, key, cover}) => {
-  const [searchInput, setSearchInput] = useState(undefined)
-
+const Result = ({id, key, cover, conditionInput, setConditionInput}) => {
+  
   return (
     <div id={id} key={key} className='result' data-cy='result'>
       <img data-cy="book-cover" src={cover} alt="Book Cover" className="book-cover" />
           <select name='condition'
-            value={searchInput} 
-            onChange={(event) => setSearchInput(event.target.value)} 
+            value={conditionInput} 
+            onChange={(event) => setConditionInput(event.target.value)} 
           >
             <option value=''>Select Book Condition</option>
             <option value='POOR'>Poor</option>
@@ -16,7 +13,7 @@ const Result = ({id, key, cover}) => {
             <option value='EXCELLENT'>Excellent</option>
           </select>
       <button 
-        data-cy='search-add-book-btn' disabled={!searchInput}> 
+        data-cy='search-add-book-btn' disabled={!conditionInput}> 
         Add this book to my shelf!
         {/* // onClick={(event)=> handleClick(event)}>Search */}
       </button>
