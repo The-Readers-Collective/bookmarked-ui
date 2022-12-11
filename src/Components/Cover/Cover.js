f1bb842a79984a485940bd2b0d529d3ff72811e7
Marianne Barton <mhood24@gmail.com>
2022-12-11 16:38:56 -0700

    Add opacity to books that are unavailable to borrow

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 import './Cover.css'
 
 const Cover = ({ cover, id, available, deleteBook }) => {
+
   return (
     <div>
     <Link to={`/${id}`}> 
-      <div data-cy='cover' className='cover' id={id}>
+      <div data-cy='cover' className='cover' id={id} style={!available ? {opacity: '0.2'} : {}}>
         <img data-cy='cover-image' className='cover-image' src={cover} alt='book name'/>
       </div>
     </Link>
