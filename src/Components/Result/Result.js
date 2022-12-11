35526b4c3b0e7849407aaab29881aa93057284df
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-11 15:07:02 -0700

    Add data-cy and className to elements

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -6,11 +6,11 @@ const Result = ({id, key, cover}) => {
   return (
     <div id={id} key={key} className='result' data-cy='result'>
       <img data-cy="book-cover" src={cover} alt="Book Cover" className="book-cover" />
-          <select name='condition'
+          <select data-cy="select-search" className="select-search" name='condition'
             value={searchInput} 
             onChange={(event) => setSearchInput(event.target.value)} 
           >
-            <option value=''>Select Book Condition</option>
+            <option data-cy="select-condition" value=''>Select Book Condition</option>
             <option value='0'>Excellent</option>
             <option value='1'>Good</option>
             <option value='2'>Poor</option>
