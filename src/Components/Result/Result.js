3602bb0c6fab4167f17fd2e4cbf2d079ad8c375f
Marianne Barton <mhood24@gmail.com>
2022-12-10 20:07:10 -0700

    Add disabled attribute to button, allow user to click condition choice

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Result = ({id, key, cover}) => {
+  const [searchInput, setSearchInput] = useState(null)
+
   return (
     <div id={id} key={key} className='result' data-cy='result'>
       <img data-cy="book-cover" src={cover} alt="Book Cover" className="book-cover" />
-      <select name='book' id='book-condition'>
-        <option value=''>Select Book Condition</option>
-        <option value='0'>Poor</option>
-        <option value='1'>Good</option>
-        <option value='2'>Excellent</option>
-      </select>
+        <label for='condition'>
+          <select name='condition'
+            value={searchInput} 
+            onChange={(event) => setSearchInput(event.target.value)} 
+          >
+            <option value=''>Select Book Condition</option>
+            <option value='0'>Excellent</option>
+            <option value='1'>Good</option>
+            <option value='2'>Poor</option>
+          </select>
+        </label>
       <button 
-        data-cy='search-add-book-btn'> 
+        data-cy='search-add-book-btn' disabled={!searchInput}> 
         Add this book to my shelf!
         {/* // onClick={(event)=> handleClick(event)}>Search */}
       </button>
