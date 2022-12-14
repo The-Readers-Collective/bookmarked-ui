16b8ad10aeb5df8baaa9e8328e6ab105e0d83348
Scott Gaines <scottdgaines@gmail.com>
2022-12-14 13:49:24 -0700

    Refactor SingleBookView.js

--- src/Pages/SingleBookView/SingleBookView.js
+++ src/Pages/SingleBookView/SingleBookView.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import { gql, useMutation, useQuery } from '@apollo/client'
-import { GET_DASHBOARD } from '../../queries'
 import './SingleBookView.css'
-
 
 const SingleBookView = ({fromShelf, id, userId }) => {
     const GET_SINGLE_BOOK = gql`
@@ -23,63 +20,58 @@ const SingleBookView = ({fromShelf, id, userId }) => {
                 }
     `;
 
-const BOOKMARK_BOOK = gql `
-     mutation createUserBook ($userId: ID!, $bookId: ID!, $status: String!){
-        createUserBook(input: {
-            userId: $userId,
-            bookId: $bookId,
-            status: $status
-        }) {
-            userBook {
-                userId
-                bookId
-                status
-        }
-            errors
+    const BOOKMARK_BOOK = gql`
+        mutation createUserBook ($userId: ID!, $bookId: ID!, $status: String!){
+            createUserBook(input: {
+                userId: $userId,
+                bookId: $bookId,
+                status: $status
+            }) {
+                userBook {
+                    userId
+                    bookId
+                    status
             }
-        }
-    `;
-const [createUserBook, {createUserBookError, createUserBookLoading}] = useMutation(BOOKMARK_BOOK, {
-    variables: {
-            userId: parseInt(userId),
-            bookId: parseInt(id),
-            status: 'BOOKMARKED'
-    }
-})
+                errors
+                }
+            }
+        `;
 
-function AddBookmark(id) {
-    if (createUserBookError) return <p>Error : {error.message}</p>
-    if (createUserBookLoading) return <p>Loading...</p>
-
-    createUserBook ({
+    const [createUserBook, {createUserBookError, createUserBookLoading}] = useMutation(BOOKMARK_BOOK, {
         variables: {
-            input: {
                 userId: parseInt(userId),
                 bookId: parseInt(id),
                 status: 'BOOKMARKED'
-            }
         }
     })
-    refetch()
-}
-console.log('userID', userId)
 
-const { loading, error, data, refetch} = useQuery(GET_SINGLE_BOOK, {variables: {id: id}})
+    function AddBookmark(id) {
+        if (createUserBookError) return <p>Error : {error.message}</p>
+        if (createUserBookLoading) return <p>Loading...</p>
 
-console.log(data)
+        createUserBook ({
+            variables: {
+                input: {
+                    userId: parseInt(userId),
+                    bookId: parseInt(id),
+                    status: 'BOOKMARKED'
+                }
+            }
+        })
+        refetch()
+    }
 
+    const { loading, error, data, refetch} = useQuery(GET_SINGLE_BOOK, {variables: {id: id}})
     
     if (error) return <p>Error : {error.message}</p>
     if (loading) return <p>Loading...</p>
 
-    // let dynamicButton;
     if (fromShelf) {
         return <button>Delete</button>
     }
 
     return (
         <div data-cy='single-book-container' className='single-book-container'>
-            <p>Single book view</p>
             <div data-cy='top-single-book-container' className='top-single-book-container'>
                 <div data-cy='cover-image-container' className='cover-image-container'>
                     <img  data-cy="book-cover"  src={data.book.bookCover} alt="Book Cover" className="book-cover" />
