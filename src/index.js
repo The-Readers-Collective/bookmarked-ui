22f8e6e26e6828023a5f60a9103f06145687e2f1
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-14 14:30:10 -0700

    Add dependancy to useEffect

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './Pages/App/App';
 import { BrowserRouter } from 'react-router-dom';
-//don't forget to re-import gql if we use it
 
 const client = new ApolloClient({
   uri: 'https://bookmarked-api.herokuapp.com/graphql',
