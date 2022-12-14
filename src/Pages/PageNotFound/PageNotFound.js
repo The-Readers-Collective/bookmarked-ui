c98aed437c40c5b1d5ba208021caf06cffd65d47
Marianne Barton <mhood24@gmail.com>
2022-12-13 19:42:18 -0700

    Add styling to pages

--- src/Pages/PageNotFound/PageNotFound.js
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import missingPage from '../../Images/teared-book.png'
 
 const PageNotFound = () => {
   return (
     <div>
-      <p>Page Not Found</p>
+      <div className='number-error container'>
+        <div className='number'>4</div>
+        <img className='error-image' src={missingPage} alt='error design' />
+        <div className='number'>4</div>
+      </div>
+      <p>Whoops! The page you are looking for cannot be found.</p>
+      <Link to="/">
+          <button className="button">Return Home</button>
+        </Link>
     </div>
   )
 }
