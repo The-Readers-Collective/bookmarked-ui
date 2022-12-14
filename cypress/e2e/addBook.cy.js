35f4e1af34ba74ab22bad9d11ad71e555b2bd87e
Morgan Berry <morganlyn.berry@gmail.com>
2022-12-13 19:28:58 -0700

    Add intercepts to tests

--- cypress/e2e/addBook.cy.js
+++ cypress/e2e/addBook.cy.js
@@ -1,47 +1,55 @@
-import addBook from '../fixtures/addBook.json'
+import AddBook from '../fixtures/AddBook.json'
 
 describe('Browsed Collection', () => {
   beforeEach(() => {
+    cy.intercept('https://bookmarked-api.herokuapp.com/graphql', AddBook)
     cy.visit('localhost:3000/add')
   })
 
     it('should be able to search a book by name or author', () => {
+      
       cy.get('[data-cy="add-book-title"]').should('be.visible')
         .get('input')
-        .type('yellow')
+        .type('Rhythm of War')
         .get('[data-cy="searched-result-button"]')
         .click()
         .get('[data-cy="searched-books-container"] > p').should('be.visible')
         .get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
         
     })
-
-    it('should see an error if a users search is invalid, ', () => {
-      cy.get('input')
-        .type('/')
-        .get('[data-cy="searched-result-button"]')
-        .click()
-        .get('[data-cy="searched-books-container"] > p')
-        .contains('No results found.')
-
-    })
-
+    
     it('should be able to add condition to a book, and hit the button to add the book to the shelf', () => {
+      
       cy.get('[data-cy="add-book-title"]').should('be.visible')
-        .get('input')
-        .type('yellow')
-        .get('[data-cy="searched-result-button"]')
-        .click()
-        .get('[data-cy="searched-books-container"] > p').should('be.visible')
-        .get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
-        .get(':nth-child(2) > [data-cy="select-search"]')
-        .select(1)
-        cy.get('#gAuXDwAAQBAJ > [data-cy="search-add-book-btn"]')
-    })
-
-    it('should be able to return back home and see the book added to users shelf, ', () => {
-      cy.get('[data-cy="return-home-text"]')
+      .get('input')
+      .type('Rhythm of War')
+      .get('[data-cy="searched-result-button"]')
       .click()
+      .get('[data-cy="searched-books-container"] > p').should('be.visible')
+      .get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
+      .get(':nth-child(2) > [data-cy="select-search"]')
+      .select(1)
+      cy.get('#QCPBDwAAQBAJ > [data-cy="book-cover"]')
+      cy.get('#QcpBDwAAQBAJ > [data-cy="search-add-book-btn"]')
     })
-
+    
+    it('should display an error message (500 status code) if books are unable to be searched', () => {
+      cy.intercept(
+        "POST",
+        "https://bookmarked-api.herokuapp.com/graphql",
+        {
+          statusCode: 500,
+          body: {
+            error: "Not Found",
+          },
+        }
+      )
+      cy.visit("http://localhost:3000/add")
+      cy.get('input')
+      .type('/')
+      .get('[data-cy="searched-result-button"]')
+      .click()
+      .get('[data-cy="searched-books-container"] > p')
+      .contains('No results found.')
+  });
 })
