a49e88674b5300698f2f0e10290a336447a38e6b
Scott Gaines <scottdgaines@gmail.com>
2022-12-14 13:28:39 -0700

    Refactor Result.js

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -7,24 +7,23 @@ const Result = ({id, cover, addBookToShelf}) => {
   return (
     <div id={id} className='result' data-cy='result'>
       <img data-cy="book-cover" src={cover} alt="Book Cover" className="book-cover" />
-          <select data-cy="select-search" className="select-search" name='condition'
-            value={conditionInput} 
-            onChange={(event) => setConditionInput(event.target.value)} 
-          >
-            <option value=''>Select Book Condition</option>
-            <option value='POOR'>Poor</option>
-            <option value='GOOD'>Good</option>
-            <option value='EXCELLENT'>Excellent</option>
-          </select>
+        <select data-cy="select-search" className="select-search" name='condition'
+          value={conditionInput} 
+          onChange={(event) => setConditionInput(event.target.value)} 
+        >
+          <option value=''>Select Book Condition</option>
+          <option value='POOR'>Poor</option>
+          <option value='GOOD'>Good</option>
+          <option value='EXCELLENT'>Excellent</option>
+        </select>
       <button 
         data-cy='search-add-book-btn' className='search-add-book-button' disabled={!conditionInput} onClick={()=> {
             addBookToShelf(id, conditionInput) 
             }
           }
       > 
-        Add this book to my shelf!
+        Add to Shelf
       </button>
-      Add to Shelf
     </div>
   )
 }
