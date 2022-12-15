e84a38fd1b1b10eaa5786921af242879377ee7cc
Stephanie Guzman <sguzman247@gmail.com>
2022-12-14 21:28:30 -0600

    Add data-cy attr + update targeted tags in Add Book spec to pass tests

--- cypress/e2e/addBook.cy.js
+++ cypress/e2e/addBook.cy.js
@@ -15,30 +15,26 @@ describe('Add Book', () => {
   })
   
   it('should be able to search a book by name or author', () => {
-    cy.get('[data-cy="add-book-title"]').should('be.visible')
-      .get('input')
-      .type('Rhythm of War')
-      .get('[data-cy="searched-result-button"]')
-      .click()
-      .get('[data-cy="searched-books-container"] > p').should('be.visible')
-      .get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
+    cy.get('[data-cy="add-book-title"]').contains('Add your books to your digital bookshelf.')
+    cy.get('[data-cy="add-book-msg"]').contains('These books will be available to lend out.')
+    cy.get('input').type('Rhythm of War')
+    cy.get('[data-cy="searched-result-button"]').click()
+    cy.get('[data-cy="searched-books-container"]').should('exist')
+    cy.get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
   })
   
   it('should be able to add condition to a book, and hit the button to add the book to the shelf', () => {
     cy.get('[data-cy="add-book-title"]').should('be.visible')
-      .get('input')
-      .type('Rhythm of War')
-      .get('[data-cy="searched-result-button"]')
-      .click()
-      .get('[data-cy="searched-books-container"] > p').should('be.visible')
-      .get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
-      .get(':nth-child(2) > [data-cy="select-search"]')
-      .select(1)
+    cy.get('input').type('Rhythm of War')
+    cy.get('[data-cy="searched-result-button"]').click()
+    cy.get('[data-cy="searched-books-container"]').should('be.visible')
+    cy.get(':nth-child(2) > [data-cy="book-cover"]').should('be.visible')
+    cy.get(':nth-child(2) > [data-cy="select-search"]').select(1)
     cy.get('#QCPBDwAAQBAJ > [data-cy="book-cover"]')
     cy.get('#QcpBDwAAQBAJ > [data-cy="search-add-book-btn"]')
   })
 
-  it('should display an error message (500 status code) if books are unable to be searched', () => {
+  it.only('should display an error message (500 status code) if books are unable to be searched', () => {
     cy.intercept(
       "POST",
       "https://bookmarked-api.herokuapp.com/graphql",
@@ -50,12 +46,9 @@ describe('Add Book', () => {
       }
     )
     cy.visit("/add")
-    cy.get('input')
-      .type('/')
-      .get('[data-cy="searched-result-button"]')
-      .click()
-      .get('[data-cy="searched-books-container"] > p')
-      .contains('No results found.')
+    cy.get('input').type('/')
+    cy.get('[data-cy="searched-result-button"]').click()
+    cy.get('[data-cy="add-book-container"] > :nth-child(2)').contains('No results found. Please modify your search and try again.')
   })
 
   it('should display a footer with a link to GitHub', () => {
--- src/Pages/AddBook/AddBook.js
+++ src/Pages/AddBook/AddBook.js
@@ -104,7 +104,7 @@ const AddBook = ({ userId }) => {
     <div className='add-book-container' data-cy='add-book-container'>
       <div className="add-book-search-container"> 
         <h3 className='title' data-cy='add-book-title'>Add your books to your digital bookshelf.</h3>
-        <p className="user-msg disclaimer">These books will be available to lend out.</p>
+        <p data-cy='add-book-msg'className="user-msg disclaimer">These books will be available to lend out.</p>
         <div className="add-book-search-input">
           <input 
             type="text" 
