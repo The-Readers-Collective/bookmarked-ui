b173aac00a933c0697549c8a578339c0d18ec3a9
Morgan Berry <morganlyn.berry@gmail.com>
2023-01-25 15:09:10 -0700

    Add classNames to buttons

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,22 +1,23 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import './About.css'
 
 const About = () => {
 
   return (
-    <section data-cy='' className=''>
+    <section data-cy='' className='about-container'>
       <h3 data-cy='page-name' className='page-name'>Welcome to Bookmarked!</h3>
       <p className=''>Here you can add books you want to share, find books you want to borrow,
  and manage your bookshelves.</p>
-      <h4>Lend</h4>
+      <h4 className='about-directions'>Lend</h4>
       <p>Add your books to lend out</p>
-      <Link to='/add'><button>Add Book</button></Link>
+      <Link to='/add'><button className='about-buttons'>Add Book</button></Link>
       <h4>Browse</h4>
       <p>Browse to your heart’s content</p>
-      <Link to='/browse'><button>Browse Books</button></Link>
+      <Link to='/browse'><button className='about-buttons'>Browse Books</button></Link>
       <h4>Manage</h4>
       <p>Manage your bookshelves</p>
-      <Link to='/manage'><button>Manage Books</button></Link>
+      <Link to='/manage'><button className='about-buttons'>Manage Books</button></Link>
     </section>
   )
 }
