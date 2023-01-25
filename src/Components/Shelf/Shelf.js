9380b0d9638ff222c197b52f3e657242febe4d6b
Stephanie Guzman <sguzman247@gmail.com>
2023-01-25 11:24:28 -0600

    Remove dead code from Shelf

--- src/Components/Shelf/Shelf.js
+++ src/Components/Shelf/Shelf.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import Cover from '../Cover/Cover'
 import './Shelf.css'
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation, Mousewheel, Keyboard } from "swiper";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import "swiper/css/scrollbar";
 
 const Shelf = ({ myShelfBooks, ownedBooks, bookmarkedBooks, owned, deleteBook, updateStatus }) => {
 
   const bookCovers = (books) => books.map(book => {
     return (
-      // <SwiperSlide className="swiper-slide">
         <Cover 
           fromShelf={true}
           cover={book.book.bookCover}
@@ -23,7 +16,6 @@ const Shelf = ({ myShelfBooks, ownedBooks, bookmarkedBooks, owned, deleteBook, u
           updateStatus={updateStatus}
           owned={owned}
         />
-      // </SwiperSlide>
     )
   }) 
 
@@ -32,43 +24,10 @@ const Shelf = ({ myShelfBooks, ownedBooks, bookmarkedBooks, owned, deleteBook, u
   return (
     <>
       <h3 data-cy={shelfName} className="shelf-name">{shelfName}</h3>
-    <div data-cy="shelf-container" className="shelf-container">
-        {/* <Swiper
-              modules={[Navigation, Mousewheel, Keyboard]}
-              spaceBetween={15}
-              loop={true}
-              initialSlide={2}
-              centeredSlides={true}
-              breakpoints={{
-                200: {
-                  slidesPerView: 1,
-                },
-                400: {
-                  slidesPerView: 2,
-                },
-                600: {
-                  slidesPerView: 3,
-                },
-                839: {
-                  slidesPerView: 4,
-                },
-                1000: {
-                  slidesPerView: 7,
-                },
-                1400: {
-                  slidesPerView: 8,
-                },
-              }}
-              slidesPerView={6}
-              navigation={true}
-              keyboard={true}
-              mousewheel={true}
-              className="all-swiper-paths"
-            > */}
-             {shelfName === 'My Books' ? bookCovers(ownedBooks) : bookCovers(bookmarkedBooks)}
-        {/* </Swiper> */}
-    </div>
-        </>
+      <div data-cy="shelf-container" className="shelf-container">
+        {shelfName === 'My Books' ? bookCovers(ownedBooks) : bookCovers(bookmarkedBooks)}
+      </div>
+    </>
   )
 }
 
