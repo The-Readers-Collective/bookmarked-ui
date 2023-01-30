ac34cd3e8f9b65e80a1d64f6730836ec943e3566
Morgan Berry <morganlyn.berry@gmail.com>
2023-01-30 08:31:15 -0700

    Add code for hamburger menu

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -34,7 +34,7 @@ const App = () => {
   }
 `;
 
-const { loading, error, data } = useQuery(GET_DASHBOARD, {fetchPolicy:"cache-and-network", pollInterval: 500})
+const { loading, error, data } = useQuery(GET_DASHBOARD, {fetchPolicy:'cache-and-network', pollInterval: 500})
 
 useEffect(() => {
   if (data && data.user) {
@@ -43,12 +43,33 @@ useEffect(() => {
 }, [setUserId, data])
 
 if (error) return <p>Error : {error.message}</p>
-if (loading) return <p className="loading-message">Loading...</p>
+if (loading) return <p className='loading-message'>Loading...</p>
 
 if (data) {
   return (
     <div>
-      <nav data-cy='nav-bar' className='nav-bar'>
+      <nav className='top-nav nav-bar'>
+        <div className='app-info'>
+          <img data-cy='logo-image' className='logo-image' src={logo} alt='logo design'/>
+          <div data-cy='app-info-container' className='app-info-container'>
+            <h3 data-cy='page-name' className='page-name'>Denver</h3>
+          </div>
+        </div>
+          <input id='menu-toggle' type='checkbox' />
+          <label className='menu-button-container' htmlFor='menu-toggle'>
+        <div className='menu-button'></div>
+        </label>
+        <ul data-cy='nav-links' className='menu nav-links'>
+          <NavLink data-cy='about-text' className='link' to='/'>About</NavLink>
+          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
+          <Link data-cy='browse-text' className='link' to='/browse'>Browse</Link>
+          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
+          <Link data-cy='add-text' className='link' to='/add'>Add</Link>
+          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
+          <Link data-cy='manage-text' className='link' to='/manage'>Manage</Link>
+        </ul>
+      </nav>
+      {/* <nav data-cy='nav-bar' className='nav-bar'>
         <div className='app-info'>
           <img data-cy='logo-image' className='logo-image' src={logo} alt='logo design' />
           <div data-cy='app-info-container' className='app-info-container'>
@@ -56,45 +77,45 @@ if (data) {
           </div>
         </div>
         <div data-cy='nav-links' className='nav-links'>
-          <NavLink data-cy="about-text" className='link' to="/">About</NavLink>
+          <NavLink data-cy='about-text' className='link' to='/'>About</NavLink>
           <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy="browse-text" className='link' to="/browse">Browse</Link>
+          <Link data-cy='browse-text' className='link' to='/browse'>Browse</Link>
           <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy="add-text" className='link' to="/add">Add</Link>
+          <Link data-cy='add-text' className='link' to='/add'>Add</Link>
           <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy="manage-text" className='link' to="/manage">Manage</Link>
+          <Link data-cy='manage-text' className='link' to='/manage'>Manage</Link>
         </div>
-      </nav>
+      </nav> */}
       <main>
         <Switch>
-          <Route exact path="/" render={() => 
+          <Route exact path='/' render={() => 
             <About 
             />
           }/>
-          <Route exact path="/manage" render={() => 
+          <Route exact path='/manage' render={() => 
             <Dashboard 
               books={data.user.userBooks}
               userId={userId}
             />
           }/>
-          <Route path="/browse" render={() => 
+          <Route path='/browse' render={() => 
             <BrowseAllBooks userId={userId} />
           }/>
-          <Route path="/add" render={() =>
+          <Route path='/add' render={() =>
             <AddBook 
               userId={userId}
             />
           }/>
-          <Route exact path="/:id" render={({match}) => 
+          <Route exact path='/:id' render={({match}) => 
             <SingleBookView id={match.params.id} userId={userId} />
           }/>
-          <Route path="*" render={() => 
+          <Route path='*' render={() => 
             <PageNotFound />
           }/>
         </Switch>
       </main>
       <footer data-cy='footer-container' className='footer-container'>
-        <p data-cy='footer' className='footer'> <a href="https://github.com/The-Readers-Collective">The Reader's Collective</a></p>
+        <p data-cy='footer' className='footer'> <a href='https://github.com/The-Readers-Collective'>The Reader's Collective</a></p>
       </footer>
     </div>
   )}
