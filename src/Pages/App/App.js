e73d6dd3d0c83a3d59e8d01d2e8ed836d2afd486
Morgan Berry <morganlyn.berry@gmail.com>
2023-01-30 09:54:17 -0700

    Add Li to menu

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { NavLink, Link, Route, Switch} from 'react-router-dom'
+import { Link, Route, Switch} from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
 import Dashboard from '../Dashboard/Dashboard'
 import BrowseAllBooks from '../BrowseAllBooks/BrowseAllBooks'
@@ -60,32 +60,12 @@ if (data) {
         <div className='menu-button'></div>
         </label>
         <ul data-cy='nav-links' className='menu nav-links'>
-          <NavLink data-cy='about-text' className='link' to='/'>About</NavLink>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='browse-text' className='link' to='/browse'>Browse</Link>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='add-text' className='link' to='/add'>Add</Link>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='manage-text' className='link' to='/manage'>Manage</Link>
+          <li><Link data-cy='about-text' className='link' to='/'>About</Link></li>
+          <li><Link data-cy='browse-text' className='link' to='/browse'>Browse</Link></li>
+          <li><Link data-cy='add-text' className='link' to='/add'>Add</Link></li>
+          <li><Link data-cy='manage-text' className='link' to='/manage'>Manage</Link></li>
         </ul>
       </nav>
-      {/* <nav data-cy='nav-bar' className='nav-bar'>
-        <div className='app-info'>
-          <img data-cy='logo-image' className='logo-image' src={logo} alt='logo design' />
-          <div data-cy='app-info-container' className='app-info-container'>
-          <h3 data-cy='page-name' className='page-name'>Denver</h3>
-          </div>
-        </div>
-        <div data-cy='nav-links' className='nav-links'>
-          <NavLink data-cy='about-text' className='link' to='/'>About</NavLink>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='browse-text' className='link' to='/browse'>Browse</Link>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='add-text' className='link' to='/add'>Add</Link>
-          <p className='divider-hidden'>&nbsp;&nbsp; | &nbsp; &nbsp;</p>
-          <Link data-cy='manage-text' className='link' to='/manage'>Manage</Link>
-        </div>
-      </nav> */}
       <main>
         <Switch>
           <Route exact path='/' render={() => 
